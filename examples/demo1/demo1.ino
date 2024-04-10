a688fa7c9b7edfd8fa8bbc60b4c74583e3d5f5f5
John V. <46841841+javos65@users.noreply.github.com>
2024-04-10 17:50:26 +0200

    demo update

--- examples/demo1/demo1.ino
+++ examples/demo1/demo1.ino
@@ -60,7 +60,7 @@ void setup() {
 
   tt=millis();
 
-  Printf_Canvas(10,10,YELLOW,2,"RGB656 Canvas %ix%i - %iKB",Canvas_width(),Canvas_height(),Canvas_width()*Canvas_height()*2); // print large text on Canvas buffer 
+  Printf_Canvas(10,10,YELLOW,2,"RGB656 Canvas %ix%i - %iKB",Canvas_width(),Canvas_height(),Canvas_width()*Canvas_height()*2/1024); // print large text on Canvas buffer 
   Printf_Canvas(Sprite_x()+10,Sprite_y()+10,WHITE,1,"ARGB8888 Sprite Area %ix%i",Sprite_width(),Sprite_height()); // print text on Canvas buffer
   Canvas_FillRect(700,100,10,300,RED,0x80);
   Canvas_FillRect(20,30,10,400,MAGENTA,0xA0);
@@ -76,24 +76,27 @@ void setup() {
 
 void loop() {
   // Speed test wit hfull grapics and 3 color types of sprite.
-uint32_t tx,tt = millis();
+uint32_t t,tx,tt = millis();
 uint16_t s,xp ,xp1;
 uint16_t yp1,yp2,yp3,yp4 = Sprite_height()/2;
 yp1=yp4;yp2=yp4;yp3=yp4;
 
 while(1) {
-      tx=micros();
-      for(int t=0;t<240;++t){
       xp+=12 ; if (xp>Sprite_width()) xp-=Sprite_width();//random(Canvas_height()/2,Canvas_height()-INVADER_Y+1);}  
       xp1+=8 ; if (xp1>Sprite_width()) xp1-=Sprite_width();//random(Canvas_height()/2,Canvas_height()-INVADER_Y+1);}  
-      Invaders1_1.xpos = xp;Invaders2_1.xpos=xp1+40;Invaders3_1.xpos=xp+80;Invaders4_1.xpos=xp1+100;
       yp1+= random(0,3); if (yp1 > Sprite_height())  yp1 -=Sprite_height();
       yp2+= random(1,8); if (yp2 > Sprite_height()) yp2 -=Sprite_height();
       yp3+= random(2,7); if (yp3 > Sprite_height())  yp3 -=Sprite_height();
       yp4+= random(3,5); if (yp4 > Sprite_height())  yp4 -=Sprite_height();
-      Invaders1_1.ypos = yp1;Invaders2_1.ypos=yp2;Invaders3_1.ypos=yp3;Invaders4_1.ypos=yp4;
 
       Sprite_ClearFrame(0x00000000); // fill buffer transparant nothing
+      for( t=0;t<10;++t) { Invaders1_1.xpos=random(32,38)+t*(Invaders1_1.width+6);Invaders1_1.ypos =10;Sprite_DrawImage(Invaders1_1); }
+      for( t=0;t<10;++t) { Invaders2_1.xpos=random(17,27)+t*(Invaders2_1.width+6);Invaders2_1.ypos =40;Sprite_DrawImage(Invaders2_1); }
+      for( t=0;t<10;++t) { Invaders3_1.xpos=random(29,34)+t*(Invaders3_1.width+6);Invaders3_1.ypos =70;Sprite_DrawImage(Invaders3_1); }
+
+      Invaders1_1.xpos = xp;Invaders2_1.xpos=xp1+40;Invaders3_1.xpos=xp+80;Invaders4_1.xpos=xp1+100;
+      Invaders1_1.ypos = yp1;Invaders2_1.ypos=yp2;Invaders3_1.ypos=yp3;Invaders4_1.ypos=yp4;
+
         Sprite_DrawImage(Invaders1_1); s=1;       // Draw SpriteImage on Sprite Canvas 8888
         Sprite_DrawImage(Invaders2_1); s=2;      // Draw SpriteImage on Sprite Canvas 1555
         Sprite_DrawImage(Invaders3_1); s=3 ;     // Draw SpriteImage on Sprite Canvas 4444
@@ -101,8 +104,7 @@ while(1) {
       Printf_Sprite(10,Sprite_height()-25,ORANGE,3,"%i:%i",millis()-tt,millis());            // print text on Sprite Canvas
       tt=millis();
       CanvasAll_DrawFrames(true);                                                // Draw Canvas and Sprite Canvas to DSI
-      }
-      DEBUGF(" Average Speed #%d %ius\n\r",s, (micros()-tx)/240);
+
     
   } // forever
 } //loop
